719c573e16c034385c033830cb7c7bac5a8673ee
Vahdettin Balum <mrbalum@dexpro.de>
2023-01-25 08:48:39 +0100

    Use Custom Exception during the initialization of softek barcode library - change class property naming

--- src/SoftekBarcodeAPI.php
+++ src/SoftekBarcodeAPI.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace DexproSolutionsGmbh\SoftekBarcodeWrapper;
 
+use DexproSolutionsGmbh\SoftekBarcodeWrapper\Exception\SoftekInitializationException;
 use DexproSolutionsGmbh\SoftekBarcodeWrapper\Model\SoftekBarcodeConfig;
 use FFI;
 
@@ -13,7 +14,7 @@ class SoftekBarcodeAPI
      *
      * @var FFI
      */
-    protected FFI $interface;
+    protected FFI $ffi;
 
     /**
      * The persistent instance of the barcode tool kit
@@ -22,16 +23,23 @@ class SoftekBarcodeAPI
      */
     protected FFI\CData $instance;
 
+    /**
+     * @param SoftekBarcodeConfig $configuration
+     * @throws SoftekInitializationException If the license is invalid or the shared library is broken or not found
+     */
     public function __construct(SoftekBarcodeConfig $configuration)
     {
-        // init the c library with his declarations and source code
-        $this->interface = FFI::cdef(file_get_contents($configuration->getHeaderFilePath()), $configuration->getSourceFilePath());
+        try {
+            // init the c library with his declarations and source code
+            $this->ffi = FFI::cdef(file_get_contents($configuration->getHeaderFilePath()), $configuration->getSourceFilePath());
+            // create a persistent instance of the interface so that we can work with the same one during operations
+            $this->instance = $this->ffi->mtCreateBarcodeInstance();
 
-        // create a persistent instance of the interface so that we can work with the same one during operations
-        $this->instance = $this->interface->mtCreateBarcodeInstance();
-
-        // license the instance for further operations
-        $this->interface->mtSetLicenseKey($this->instance, $configuration->getLicenseKey());
+            // license the instance for further operations
+            $this->ffi->mtSetLicenseKey($this->instance, $configuration->getLicenseKey());
+        } catch (\Throwable $e) {
+            throw new SoftekInitializationException($e->getMessage(), "", $e);
+        }
     }
 
 }
